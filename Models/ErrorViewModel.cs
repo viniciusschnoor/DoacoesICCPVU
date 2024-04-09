458f32bf46f875addf685aa17a56b318a63f6801
Vinicius Schnoor <vschnooor@outlook.com>
2024-04-09 17:25:00 -0300

    Run/Watch Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DoacoesICCPVU.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
