ee1f3ff9b5547806f9eae9ad81a12c1917e7ea58
viniciusschnoor <72426583+viniciusschnoor@users.noreply.github.com>
2024-04-10 23:35:57 -0300

    Register new donations

--- Controllers/DoacaoController.cs
+++ Controllers/DoacaoController.cs
@@ -1,4 +1,5 @@
 ﻿using DoacoesICCPVU.Contexts;
+using DoacoesICCPVU.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DoacoesICCPVU.Controllers
@@ -17,6 +18,29 @@ namespace DoacoesICCPVU.Controllers
             ViewData["Title"] = "Doações";
             var doacoes = _context.Doacoes.ToList();
             return View(doacoes);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Nova Doação";
+            return View("Form");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Doacao doacao)
+        {
+            if (_context.Doacoes.Count() > 0)
+            {
+                var ultimaDoacaoId = _context.Doacoes.OrderBy(d => d.Id).Last().Id;
+                doacao.NumeroOficio = (ultimaDoacaoId + 1).ToString();
+            } else
+            {
+                doacao.NumeroOficio = 1.ToString();
+            }
+            doacao.CriadoEm = DateTime.Now;
+            _context.Doacoes.Add(doacao);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
